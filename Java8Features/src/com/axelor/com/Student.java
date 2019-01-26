0c4dcced42430b50e91f2ce48daeb4ed5515316a
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-26 20:47:06 +0530

    First java code

---
+++ Java8Features/src/com/axelor/com/Student.java
@@ -0,0 +1,18 @@
+package com.axelor.com;
+
+public class Student {
+
+	
+	static void studentName() {
+		System.out.println("Abhinav Devkar");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Static Method Call !!!");
+		studentName();
+		
+		
+	}
+
+}
